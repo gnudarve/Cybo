119f4d4fa03dcccfa554cd3b06e2c50cbfbd8ebb
Buck McGibbony <buck@wisewerks.com>
2023-06-30 12:48:33 -0700

    Initial commit

---
+++ GameScreen.Designer.vb
@@ -0,0 +1,398 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GameScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.cmdRollDice = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Tally1 = New System.Windows.Forms.Label()
+        Me.Tally2 = New System.Windows.Forms.Label()
+        Me.Tally3 = New System.Windows.Forms.Label()
+        Me.tally4 = New System.Windows.Forms.Label()
+        Me.tally8 = New System.Windows.Forms.Label()
+        Me.Tally7 = New System.Windows.Forms.Label()
+        Me.tally6 = New System.Windows.Forms.Label()
+        Me.Tally5 = New System.Windows.Forms.Label()
+        Me.Tally12 = New System.Windows.Forms.Label()
+        Me.Tally11 = New System.Windows.Forms.Label()
+        Me.Tally10 = New System.Windows.Forms.Label()
+        Me.Tally9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.Control
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(362, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 108)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(362, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 108)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(362, 225)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(122, 108)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "3"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(362, 333)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(122, 108)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "4"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(484, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 108)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "5"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Black
+        Me.Label6.Location = New System.Drawing.Point(484, 117)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(122, 108)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "6"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(484, 225)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(122, 108)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "7"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Black
+        Me.Label8.Location = New System.Drawing.Point(484, 333)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(122, 108)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "8"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Black
+        Me.Label9.Location = New System.Drawing.Point(606, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(122, 108)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "9"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label10.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Black
+        Me.Label10.Location = New System.Drawing.Point(606, 117)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(122, 108)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "10"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label11
+        '
+        Me.Label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label11.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Black
+        Me.Label11.Location = New System.Drawing.Point(606, 225)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(122, 108)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "11"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label12.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Black
+        Me.Label12.Location = New System.Drawing.Point(606, 333)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(122, 108)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "12"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'cmdRollDice
+        '
+        Me.cmdRollDice.Font = New System.Drawing.Font("Arial Rounded MT Bold", 24.0!)
+        Me.cmdRollDice.Location = New System.Drawing.Point(30, 27)
+        Me.cmdRollDice.Name = "cmdRollDice"
+        Me.cmdRollDice.Size = New System.Drawing.Size(297, 62)
+        Me.cmdRollDice.TabIndex = 12
+        Me.cmdRollDice.Text = "Roll Dice"
+        Me.cmdRollDice.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Tally1
+        '
+        Me.Tally1.AutoSize = True
+        Me.Tally1.Location = New System.Drawing.Point(369, 90)
+        Me.Tally1.Name = "Tally1"
+        Me.Tally1.Size = New System.Drawing.Size(16, 17)
+        Me.Tally1.TabIndex = 13
+        Me.Tally1.Text = "0"
+        Me.Tally1.Visible = False
+        '
+        'Tally2
+        '
+        Me.Tally2.AutoSize = True
+        Me.Tally2.Location = New System.Drawing.Point(369, 199)
+        Me.Tally2.Name = "Tally2"
+        Me.Tally2.Size = New System.Drawing.Size(16, 17)
+        Me.Tally2.TabIndex = 14
+        Me.Tally2.Text = "0"
+        Me.Tally2.Visible = False
+        '
+        'Tally3
+        '
+        Me.Tally3.AutoSize = True
+        Me.Tally3.Location = New System.Drawing.Point(369, 305)
+        Me.Tally3.Name = "Tally3"
+        Me.Tally3.Size = New System.Drawing.Size(16, 17)
+        Me.Tally3.TabIndex = 15
+        Me.Tally3.Text = "0"
+        Me.Tally3.Visible = False
+        '
+        'tally4
+        '
+        Me.tally4.AutoSize = True
+        Me.tally4.Location = New System.Drawing.Point(369, 415)
+        Me.tally4.Name = "tally4"
+        Me.tally4.Size = New System.Drawing.Size(16, 17)
+        Me.tally4.TabIndex = 16
+        Me.tally4.Text = "0"
+        Me.tally4.Visible = False
+        '
+        'tally8
+        '
+        Me.tally8.AutoSize = True
+        Me.tally8.Location = New System.Drawing.Point(490, 415)
+        Me.tally8.Name = "tally8"
+        Me.tally8.Size = New System.Drawing.Size(16, 17)
+        Me.tally8.TabIndex = 20
+        Me.tally8.Text = "0"
+        Me.tally8.Visible = False
+        '
+        'Tally7
+        '
+        Me.Tally7.AutoSize = True
+        Me.Tally7.Location = New System.Drawing.Point(490, 305)
+        Me.Tally7.Name = "Tally7"
+        Me.Tally7.Size = New System.Drawing.Size(16, 17)
+        Me.Tally7.TabIndex = 19
+        Me.Tally7.Text = "0"
+        Me.Tally7.Visible = False
+        '
+        'tally6
+        '
+        Me.tally6.AutoSize = True
+        Me.tally6.Location = New System.Drawing.Point(490, 199)
+        Me.tally6.Name = "tally6"
+        Me.tally6.Size = New System.Drawing.Size(16, 17)
+        Me.tally6.TabIndex = 18
+        Me.tally6.Text = "0"
+        Me.tally6.Visible = False
+        '
+        'Tally5
+        '
+        Me.Tally5.AutoSize = True
+        Me.Tally5.Location = New System.Drawing.Point(490, 90)
+        Me.Tally5.Name = "Tally5"
+        Me.Tally5.Size = New System.Drawing.Size(16, 17)
+        Me.Tally5.TabIndex = 17
+        Me.Tally5.Text = "0"
+        Me.Tally5.Visible = False
+        '
+        'Tally12
+        '
+        Me.Tally12.AutoSize = True
+        Me.Tally12.Location = New System.Drawing.Point(612, 415)
+        Me.Tally12.Name = "Tally12"
+        Me.Tally12.Size = New System.Drawing.Size(16, 17)
+        Me.Tally12.TabIndex = 24
+        Me.Tally12.Text = "0"
+        Me.Tally12.Visible = False
+        '
+        'Tally11
+        '
+        Me.Tally11.AutoSize = True
+        Me.Tally11.Location = New System.Drawing.Point(612, 305)
+        Me.Tally11.Name = "Tally11"
+        Me.Tally11.Size = New System.Drawing.Size(16, 17)
+        Me.Tally11.TabIndex = 23
+        Me.Tally11.Text = "0"
+        Me.Tally11.Visible = False
+        '
+        'Tally10
+        '
+        Me.Tally10.AutoSize = True
+        Me.Tally10.Location = New System.Drawing.Point(612, 199)
+        Me.Tally10.Name = "Tally10"
+        Me.Tally10.Size = New System.Drawing.Size(16, 17)
+        Me.Tally10.TabIndex = 22
+        Me.Tally10.Text = "0"
+        Me.Tally10.Visible = False
+        '
+        'Tally9
+        '
+        Me.Tally9.AutoSize = True
+        Me.Tally9.Location = New System.Drawing.Point(612, 90)
+        Me.Tally9.Name = "Tally9"
+        Me.Tally9.Size = New System.Drawing.Size(16, 17)
+        Me.Tally9.TabIndex = 21
+        Me.Tally9.Text = "0"
+        Me.Tally9.Visible = False
+        '
+        'GameScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(741, 455)
+        Me.Controls.Add(Me.Tally12)
+        Me.Controls.Add(Me.Tally11)
+        Me.Controls.Add(Me.Tally10)
+        Me.Controls.Add(Me.Tally9)
+        Me.Controls.Add(Me.tally8)
+        Me.Controls.Add(Me.Tally7)
+        Me.Controls.Add(Me.tally6)
+        Me.Controls.Add(Me.Tally5)
+        Me.Controls.Add(Me.tally4)
+        Me.Controls.Add(Me.Tally3)
+        Me.Controls.Add(Me.Tally2)
+        Me.Controls.Add(Me.Tally1)
+        Me.Controls.Add(Me.cmdRollDice)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "GameScreen"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cybo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents Label4 As Windows.Forms.Label
+    Friend WithEvents Label5 As Windows.Forms.Label
+    Friend WithEvents Label6 As Windows.Forms.Label
+    Friend WithEvents Label7 As Windows.Forms.Label
+    Friend WithEvents Label8 As Windows.Forms.Label
+    Friend WithEvents Label9 As Windows.Forms.Label
+    Friend WithEvents Label10 As Windows.Forms.Label
+    Friend WithEvents Label11 As Windows.Forms.Label
+    Friend WithEvents Label12 As Windows.Forms.Label
+    Friend WithEvents cmdRollDice As Windows.Forms.Button
+    Friend WithEvents Timer1 As Windows.Forms.Timer
+    Friend WithEvents Tally1 As Windows.Forms.Label
+    Friend WithEvents Tally2 As Windows.Forms.Label
+    Friend WithEvents Tally3 As Windows.Forms.Label
+    Friend WithEvents tally4 As Windows.Forms.Label
+    Friend WithEvents tally8 As Windows.Forms.Label
+    Friend WithEvents Tally7 As Windows.Forms.Label
+    Friend WithEvents tally6 As Windows.Forms.Label
+    Friend WithEvents Tally5 As Windows.Forms.Label
+    Friend WithEvents Tally12 As Windows.Forms.Label
+    Friend WithEvents Tally11 As Windows.Forms.Label
+    Friend WithEvents Tally10 As Windows.Forms.Label
+    Friend WithEvents Tally9 As Windows.Forms.Label
+End Class
---
+++ GameScreen.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Drawing
+Imports System.Security.Cryptography
+Imports System.Windows.Forms
+
+Public Class GameScreen
+
+    Private Const m_nDiceSides As Integer = 12
+    Private Const m_nRollsPerTurn As Integer = 4
+
+    Private m_cOpenColor = Color.AliceBlue
+    Private m_cSetColor = Color.DodgerBlue
+    Private m_cInertColor = SystemColors.Control
+
+    Private m_rngCsp As RNGCryptoServiceProvider = New RNGCryptoServiceProvider()
+
+    Private m_Board As Label()
+    Private m_Tally As Label()
+
+    Private MyTurn As New TurnInfo With {
+            .nResults = New Integer(m_nRollsPerTurn - 1) {}
+        }
+
+    Private Structure TurnInfo
+        Public nRollNumber As Integer
+        Public nPoints As Integer
+        Public nEstablishedLine As Integer
+        Public nResults As Integer()
+    End Structure
+
+    Private CyboLines As Byte()() = New Byte()() {
+        New Byte() {1, 5, 9},
+        New Byte() {2, 6, 10},
+        New Byte() {3, 7, 11},
+        New Byte() {4, 8, 12},
+        New Byte() {1, 2, 3, 4},
+        New Byte() {5, 6, 7, 8},
+        New Byte() {9, 10, 11, 12},
+        New Byte() {2, 7, 12},
+        New Byte() {1, 6, 11},
+        New Byte() {3, 6, 9},
+        New Byte() {4, 7, 10}
+    }
+
+    Private Sub GameScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        m_Board = {Label1, Label2, Label3, Label4, Label5, Label6, Label7, Label8, Label9, Label10, Label11, Label12}
+        m_Tally = {Tally1, Tally2, Tally3, tally4, Tally5, tally6, Tally7, tally8, Tally9, Tally10, Tally11, Tally12}
+
+        ClearTurn()
+
+    End Sub
+
+
+    Private Function IsDuplicate(theTurn As TurnInfo) As Boolean
+
+        For nRoll As Integer = 0 To theTurn.nRollNumber - 1
+            If theTurn.nResults(nRoll) = theTurn.nResults(theTurn.nRollNumber) Then
+                Return True
+            End If
+        Next
+
+        Return False
+
+    End Function
+
+    Private Sub RollTest_ShowLines()
+
+        Static nLastRoll As Integer = 0
+        Dim nRoll As Integer
+        Dim nEval As Integer
+
+        'roll dice
+        Do
+            nRoll = RollDice(m_nDiceSides)
+        Loop While nRoll = nLastRoll
+        nLastRoll = nRoll
+
+        MyTurn.nResults(MyTurn.nRollNumber) = nRoll
+
+        ' check for same number hit twice
+        If IsDuplicate(MyTurn) Then
+            Debug.WriteLine("Duplicate number.")
+            nEval = -1
+        Else
+            nEval = EvalResults(MyTurn)
+        End If
+
+
+        Select Case nEval
+            Case -1
+                Debug.WriteLine("Out of line.")
+                MyTurn.nPoints = 0
+
+            Case 0
+                'keep going 
+                MyTurn.nRollNumber += 1
+
+            Case Else
+                Debug.WriteLine("Line {0}: Turn is over, you scored: {1}!", MyTurn.nEstablishedLine, MyTurn.nPoints)
+                MyTurn.nPoints = nEval
+
+        End Select
+
+        SetCellColor(nRoll, m_cSetColor)
+
+        'show all available lines
+
+    End Sub
+
+    Private Sub RollTest()
+
+        Static nLastRoll As Integer = 0
+        Dim nRoll As Integer
+
+        'ClearBoard()
+        SetCellColor(nLastRoll, m_cInertColor)
+
+        'Do
+        nRoll = RollDice(m_nDiceSides)
+        'Loop While nRoll = nLastRoll
+        nLastRoll = nRoll
+
+        SetCellColor(nRoll, m_cSetColor)
+        m_Tally(nRoll - 1).Text = (CInt(m_Tally(nRoll - 1).Text) + 1).ToString
+
+    End Sub
+
+    ' Evaluate current turn based on CyboLines map
+    ' 
+    ' Returns:
+    '     0 - Turn can continue, no points awarded yet
+    '    -1 - Turn is over, results are not in a line
+    '     n - Points this turn is awarded
+    Private Function EvalResults(theTurn As TurnInfo) As Integer
+        Dim nLineNum As Integer = 0
+        Dim nLineHits As Integer = 0
+        Dim nPoints As Integer = 0
+
+        ' see if the rolls are in the same line
+        For nLineNum = 0 To CyboLines.GetLength(0) - 1
+            Dim nHits As Integer = 0
+            For Each nNumber As Integer In CyboLines(nLineNum)
+                For nRoll As Integer = 0 To theTurn.nRollNumber
+                    If theTurn.nResults(nRoll) = nNumber Then
+                        nHits += 1
+                    End If
+                Next
+            Next
+
+            If nHits = theTurn.nRollNumber + 1 Then
+                Dim sSequence As String = ""
+                For Each nNumber As Integer In CyboLines(nLineNum)
+                    sSequence += (nNumber.ToString() + " ")
+                Next
+                Debug.WriteLine("Possible Line {0}: {1}", nLineNum + 1, sSequence)
+
+                If nHits > nLineHits Then
+                    nLineHits = nHits
+                    theTurn.nEstablishedLine = nLineNum
+                End If
+            End If
+        Next
+
+        ' evaluate points
+        Select Case theTurn.nRollNumber
+            Case 0, 1
+                nPoints = 0
+            Case 2
+                nPoints = 3
+            Case 3
+                nPoints = 16
+        End Select
+
+        Return nPoints
+
+    End Function
+
+
+    ' This method simulates a roll of the dice. The input parameter is the
+    ' number of sides of the dice.
+    Private Function RollDice(nDiceSides As Integer) As Integer
+        Static oRandom As New Random(Now.Millisecond)
+        Return oRandom.Next(1, nDiceSides + 1)
+    End Function
+
+    ' This method simulates a roll of the dice. The input parameter is the
+    ' number of sides of the dice.
+    Private Function RollDice(ByVal numberSides As UInt16) As UInt16
+        Dim randomBytes As Byte() = New Byte(1) {}
+        m_rngCsp.GetBytes(randomBytes)
+        Return CUShort((BitConverter.ToUInt16(randomBytes, 0) Mod numberSides) + 1)
+    End Function
+
+    Private Sub cmdRollDice_Click(sender As Object, e As EventArgs) Handles cmdRollDice.Click
+        RollTest_ShowLines()
+        'Timer1.Enabled = Not Timer1.Enabled
+    End Sub
+
+
+    Private Sub ClearTurn()
+        For i As Integer = 1 To m_nDiceSides
+            SetCellColor(i, m_cOpenColor)
+        Next
+
+        MyTurn.nEstablishedLine = -1
+        MyTurn.nPoints = 0
+        MyTurn.nRollNumber = 0
+        For i As Integer = 0 To m_nRollsPerTurn - 1
+            MyTurn.nResults(i) = 0
+        Next
+    End Sub
+
+    Private Sub SetCellColor(nCellNumber As Integer, nTargetColor As Color)
+        If nCellNumber >= 1 AndAlso nCellNumber <= m_Board.Length Then
+            m_Board(nCellNumber - 1).BackColor = nTargetColor
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        RollTest()
+        RollTest_ShowLines()
+    End Sub
+
+End Class
