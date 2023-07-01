7463e1a0216382dd460ad1c28af9910760081334
Buck McGibbony <buck@wisewerks.com>
2023-06-30 18:36:15 -0700

    Beta

--- GameScreen.Designer.vb
+++ GameScreen.Designer.vb
@@ -23,6 +23,7 @@ Partial Class GameScreen
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(GameScreen))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -385,6 +386,7 @@ Partial Class GameScreen
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "GameScreen"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
